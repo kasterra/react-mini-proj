4d9aee88a85f5070f89c1af12f2e91fca9363e1c
kasterra <kasterra@naver.com>
2022-05-13 10:07:00 +0900

    ✨ feat(chart): implemented chart screen

--- package/crypto-tracker/src/Pages/Chart.tsx
+++ package/crypto-tracker/src/Pages/Chart.tsx
@@ -1,4 +1,79 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import ApexChart from "react-apexcharts";
+import { fetchCoinHistory } from "../api";
+
+interface IHistory {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const { coinId } = useOutletContext() as { coinId: string };
+  const { isLoading, data } = useQuery<IHistory[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data!.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: "100%",
+              width: "100%",
+              toolbar: {
+                show: false,
+              },
+            },
+            xaxis: {
+              type: "datetime",
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: { show: false },
+              categories: data!.map((price) => price.time_close),
+            },
+            yaxis: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"] },
+            },
+            colors: ["#0fbc89"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 export default Chart;
